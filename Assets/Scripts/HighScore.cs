e21cba92e0db275fb61215f04cae921d3c713c5a
xmarshall39 <48485277+xmarshall39@users.noreply.github.com>
2019-03-22 11:15:31 -0400

    Update

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -11,7 +11,7 @@ public class HighScore : MonoBehaviour
     
     public int defaultScore = 1000;
     public int[] highScores = new int[10];
-    
+    int new_score;
     //Manager must last through gameplay
     private void Awake()
     {
@@ -21,7 +21,7 @@ public class HighScore : MonoBehaviour
   
     private void Start()
     {
-        player = GetComponent<Player>();
+        
         for (int i = 0; i < 10; ++i)
         {
             string pref = "HighScore" + i.ToString();
@@ -30,17 +30,26 @@ public class HighScore : MonoBehaviour
             else
                 highScores[i] = defaultScore;
         }
+
+        for (int i = 0; i < 10; ++i)
+        {
+            print(highScores[i]);
+        }
     }
 
     private void Update()
     {
-        int new_score = 0;
+        
         //Keep track of the high score during gameplay
-        if (SceneManager.Equals("GameScene", SceneManager.GetActiveScene()))
+        if (SceneManager.Equals(SceneManager.GetSceneByName("GameScene"), SceneManager.GetActiveScene()))
+        {
+            player = GetComponent<Player>();
             new_score = player.score;
+        }
+            
         
         //Change the highest score to be the new_score
-        else if(SceneManager.Equals("HighScoreScreen", SceneManager.GetActiveScene()))
+        else if(SceneManager.Equals(SceneManager.GetSceneByName("GameOver"), SceneManager.GetActiveScene()))
             for(int i = 0; i < 10; ++i)
             {
                 string pref = "HighScore" + i.ToString();
@@ -48,6 +57,8 @@ public class HighScore : MonoBehaviour
                 {
                     highScores[i] = new_score;
                     PlayerPrefs.SetInt(pref, new_score);
+                    print(highScores[i]);
+                    print(PlayerPrefs.GetInt(pref));
                     break;
                 }
                     
